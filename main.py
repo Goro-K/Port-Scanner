8f1fa7d611175455e83e1c3f398cffaa62c62a16
Goro-K <romankizilt@gmail.com>
2024-04-10 16:45:11 +0200

    add error handling

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import socket
 
+# Function to get all the information about the IP address (host, alias, IP)
 def adressInfo(ip):
     try:
         return socket.gethostbyaddr(ip)
     except socket.herror:
         return "No host found"
-    
+
 def askForIP():
     try:
         return input("Enter the IP address: ")
@@ -15,11 +16,22 @@ def askForIP():
 ip = askForIP()
 host = adressInfo(ip)
 
-for port in range(65535):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = sock.connect_ex((ip, port))
-    if result == 0:
-        print("Port " + str(port) + " is open")
-    sock.close()
+print ("Scanning host: " + str(host[0])) 
 
-print("Host: " + str(host))
+# Port scanner
+
+try:
+    for port in range(65535):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((ip, port))
+        if result == 0:
+            print ("Port {} is open".format(port))
+        sock.close()
+
+except KeyboardInterrupt:
+    print("Exiting program")
+    exit()
+
+except socket.error:
+    print("Could not connect to server")
+    exit()
